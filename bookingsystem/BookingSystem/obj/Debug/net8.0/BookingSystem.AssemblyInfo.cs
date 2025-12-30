66cea4be4adf8f999f4851f0fc65a19f5215fadc
ALintjarn <axellintjarn40@gmail.com>
2025-12-30 20:25:25 +0100

    Trial complete. Now it's time to start.

--- bookingsystem/BookingSystem/obj/Debug/net8.0/BookingSystem.AssemblyInfo.cs
+++ bookingsystem/BookingSystem/obj/Debug/net8.0/BookingSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afcd77c9d657b01a7eefbe46b7c54f271240a28")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
